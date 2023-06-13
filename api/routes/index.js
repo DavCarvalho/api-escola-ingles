9f6f2e68eda356f73a3c35ba0059ab23fca41f2c
Davi Carvalho <davircsp@hotmail.com>
2023-06-13 20:32:13 -0300

    escola_ingles-api

---
+++ api/routes/index.js
@@ -0,0 +1,14 @@
+const bodyParser = require('body-parser')
+ 
+const pessoas = require('./pessoasRoutes')
+const niveis = require('./niveisRoutes')
+const turmas = require('./turmasRoutes')
+
+module.exports = app => {
+  app.use(
+    bodyParser.json(),
+    pessoas,
+    niveis,
+    turmas
+  )
+}
